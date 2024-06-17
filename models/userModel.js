1f200edfeba4496c942a269946a155914bb124bb
AJAYKUMAR0313 <133021739+AJAYKUMAR0313@users.noreply.github.com>
2024-06-17 22:39:18 +0530

    update

--- models/userModel.js
+++ models/userModel.js
@@ -10,9 +10,9 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide your email'],
-    validate: [validator.isEmail, 'Please provide a valid email'],
     unique: true,
     lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: {
     type: String,
