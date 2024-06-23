56c62233fbc719a190550744eb17d99ca66fee80
AJAYKUMAR0313 <133021739+AJAYKUMAR0313@users.noreply.github.com>
2024-06-23 12:12:37 +0530

    added user login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,29 @@
-exports.getAllUsers = (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/userModel');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  // const features = new APIFeatures(Tour.find(), req.query)
+  //   .filter()
+  //   .sort()
+  //   .limitFields()
+  //   .pagination();
+  // console.log('Final Query:', features.query);
+  const users = await User.find();
+
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: 8,
+    select: false,
     // validate: [
     //   validator.isStrongPassword,
     //   'Please provide a stronger password',
@@ -30,6 +32,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
+      //This only works on save!!
       validator: function (el) {
         // `this` only points to the current document on NEW document creation
         return el === this.password;
@@ -38,6 +41,25 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //hash the passord with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete Password confirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
